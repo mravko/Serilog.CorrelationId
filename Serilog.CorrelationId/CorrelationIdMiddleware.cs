f38ee82c77ab9f047e1fccf795b13ef724fc0a13
Marko Vuckovik <vuckovikmarko@outlook.com>
2019-08-17 22:11:03 +0200

    Add project files.

---
+++ Serilog.CorrelationId/CorrelationIdMiddleware.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Http;
+using Serilog.Context;
+using System;
+using System.Threading.Tasks;
+
+namespace Serilog.CorrelationId
+{
+    public class CorrelationIdMiddleware
+    {
+        private const string CORRELATIONID = "X-Correlation-ID";
+
+        private readonly RequestDelegate _next;
+
+        public CorrelationIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            var correlationId = Guid.NewGuid().ToString();
+
+            if (httpContext.Request.Headers.ContainsKey(CORRELATIONID))
+            {
+                correlationId = httpContext.Request.Headers[CORRELATIONID].ToString();
+            }
+
+            httpContext.Response.OnStarting(state =>
+            {
+                if (httpContext.Response.Headers.ContainsKey(CORRELATIONID))
+                {
+                    httpContext.Response.Headers[CORRELATIONID] = correlationId;
+                }
+                else
+                {
+                    httpContext.Response.Headers.Add(CORRELATIONID, correlationId);
+                }
+                return Task.FromResult(0);
+            }, httpContext);
+
+            using (LogContext.PushProperty(CORRELATIONID, correlationId))
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
+
+
---
+++ Serilog.CorrelationId/CorrelationIdMiddlewareExtension.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Serilog.CorrelationId
+{
+    public static class CorrelationIdMiddlewareExtension
+    {
+        /// <summary>
+        /// Middleware that sets X-Correlation-ID.
+        /// If it is present in request headers as X-Correlation-ID it will be taken from there
+        /// Otherwise, it is auto-generated.
+        /// </summary>
+        /// <param name="builder"></param>
+        public static void UseCorrelationId(this IApplicationBuilder builder)
+        {
+            builder.UseMiddleware<CorrelationIdMiddleware>();
+        }
+    }
+}
+
+
